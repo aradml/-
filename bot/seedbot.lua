39d082ccb7760a345cae16f9615e730cd9569e6b
aradml <power.good2016@gmail.com>
2016-06-06 15:11:23 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {159220308},--Sudo users
+    sudo_users = {201736995},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
